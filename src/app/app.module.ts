a9f0c20558ed0b7569d7a1d8f735c5fcde119d05
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2025-08-18 13:41:41 +0530

    HTML and CSS Learning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { CatalogComponent } from './catalog/catalog.component';
-import { SiteHeaderComponent } from './site-header/site-header.component';
-import { ProductDetailsComponent } from './product-details/product-details.component';
 import { HttpClientModule } from '@angular/common/http';
-import { CartComponent } from './cart/cart.component';
 import { AppRoutingModule } from './app-routing.module';
-import { SignInComponent } from './user/sign-in/sign-in.component';
-import { TemplateFormControlsComponent } from './template-form-controls/template-form-controls.component';
-import { SearchComponent } from './search/search.component';
-import { CART_SERVICE_TOKEN, CartService } from './cart/cart.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    CatalogComponent,
-    CartComponent,
-    SiteHeaderComponent,
-    ProductDetailsComponent,
-    SignInComponent,
-    TemplateFormControlsComponent,
-    SearchComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -33,10 +16,6 @@ import { CART_SERVICE_TOKEN, CartService } from './cart/cart.service';
     HttpClientModule,
     FormsModule
   ],
-  providers: [{
-    provide: CART_SERVICE_TOKEN,
-    useClass: CartService
-  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
